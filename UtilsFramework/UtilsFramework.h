d92eb497750fe86f13143a085741437d342eb3c9
Manoj Katragadda <manoj@webileapps.com>
2018-07-11 13:43:39 +0530

    Initial Commit

---
+++ UtilsFramework/UtilsFramework.h
@@ -0,0 +1,19 @@
+//
+//  UtilsFramework.h
+//  UtilsFramework
+//
+//  Created by Manoj Katragadda on 11/07/18.
+//  Copyright © 2018 webileapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UtilsFramework.
+FOUNDATION_EXPORT double UtilsFrameworkVersionNumber;
+
+//! Project version string for UtilsFramework.
+FOUNDATION_EXPORT const unsigned char UtilsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UtilsFramework/PublicHeader.h>
+
+
